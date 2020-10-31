a568ffe78d238ab8b74a9c9c0a6d0c9d796c6857
Saeed Amin <s4ied.amin@gmail.com>
2020-10-31 06:14:54 +0100

    form components complete

--- src/Components/From.tsx
+++ src/Components/From.tsx
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { numConverter } from "../Utils/converter";
 import "./Form.css";
 
 interface FormProps {}
 const Form: React.FC<FormProps> = () => {
+  const [phrase, setPhrase] = useState("");
+  const onSubmit = () => {
+    const val =
+      (document.getElementById("input-number") as HTMLInputElement)?.value ||
+      "";
+    console.log({ val });
+    if (val === "") setPhrase("");
+    else setPhrase(numConverter(val));
+  };
+
   return (
     <>
       <form>
-        <input type="text" className="input" />
-        <button type="button" className="submit">
+        <input type="text" className="input" id="input-number" />
+        <button type="button" className="submit" onClick={onSubmit}>
           Submit
         </button>
       </form>
       <div className="phrase">
-        <small>English Phrase: </small>
-        <strong>four</strong>
+        <strong>English Phrase: </strong>
+        <small>{phrase}</small>
       </div>
     </>
   );
